1069ffc7018fc5d5a3136b0eb1180f81353fc972
Hesham Elshafey <helshafe@u.rochester.edu>
2025-04-08 15:04:42 -0400

    created description package

---
+++ src/hexabot_description/launch/display.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    urdf_tutorial_path = FindPackageShare('hexabot_description')
+    default_model_path = PathJoinSubstitution([
+    FindPackageShare('hexabot_description'),
+    'urdf',
+    'hexabot.urdf.xacro'])
+    default_rviz_config_path = PathJoinSubstitution([urdf_tutorial_path, 'rviz', 'urdf.rviz'])
+    # These parameters are maintained for backwards compatibility
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+                                    description='Flag to enable joint_state_publisher_gui')
+    ld.add_action(gui_arg)
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                     description='Absolute path to rviz config file')
+    ld.add_action(rviz_arg)
+
+    # This parameter has changed its meaning slightly from previous versions
+    ld.add_action(DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                        description='Path to robot urdf file relative to urdf_tutorial package'))
+
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'hexabot_description',
+            'urdf_package_path': LaunchConfiguration('model'),
+            'rviz_config': LaunchConfiguration('rvizconfig'),
+            'jsp_gui': LaunchConfiguration('gui')}.items()
+    ))
+
+    return ld
+
+
+
